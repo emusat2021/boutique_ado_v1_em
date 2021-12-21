6d24cc5039a510fd59bf62126c71becf45cac264
Eugenia Musat <musat.eugenia@gmail.com>
2021-12-21 22:11:43 +0100

    move ALLOWED_HOSTS to private_config

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+try:
+    from . import private_config
+    ALLOWED_HOSTS = private_config.ALLOWED_HOSTS
+except Exception:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
